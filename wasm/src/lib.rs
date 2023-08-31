a5b07c5929462e7457b9b0aee3c2e3121f6dcdf2
Erik Kinnunen <erik.kinn@gmail.com>
2023-08-31 22:49:51 +0300

    wasm: add additional information to Cargo.toml
    Add information like the LICENSE, collaborators and the description.
    
    Also add a comment to the exported `roll` function with an example.

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -2,6 +2,9 @@ use droll::{base_prng_engine, interpreter::eval, parser::parse};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
+/// Calculates the total sum from the provided dice notation, the roll result.
+/// @example
+/// roll("1d20+10"); // e.g. 27
 pub fn roll(input: &str) -> Result<isize, String> {
     match parse(input) {
         Ok(parse_tree) => Ok(eval(base_prng_engine)(parse_tree)),
